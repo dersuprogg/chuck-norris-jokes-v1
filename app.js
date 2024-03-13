1843387e2d06937c684eb0bf5994edfae02563da
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-14 00:26:49 +0500

    Initial and final commit :D

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+const jokeEl = document.querySelector(".joke");
+const getJokeBtn = document.querySelector(".btn");
+function getJoke() {
+  const url = "https://api.chucknorris.io/jokes/random";
+  const req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      const joke = data.value;
+      jokeEl.textContent = joke;
+    }
+  };
+
+  req.send();
+}
+
+getJokeBtn.addEventListener("click", getJoke);
